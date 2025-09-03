46a35202c0b750959035dc119f7ef880f7579f7f
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-09-03 21:52:18 +0500

    Smart Reply feature added

--- InboxIQ/src/SmartReply.js
+++ InboxIQ/src/SmartReply.js
@@ -1,6 +1,8 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { JsonOutputParser } from "@langchain/core/output_parsers";
 
-// make chain and export 
+// make chain and export, call response when email loads 
 
 const googleApikey = import.meta.env.VITE_GOOGLE_API_KEY
 const llm = new ChatGoogleGenerativeAI({
@@ -8,4 +10,25 @@ const llm = new ChatGoogleGenerativeAI({
     apiKey: googleApikey
 })
 
-const systemTemp = ``
+const systemTemp = `You are an AI assistant for customer support. 
+You will be given:
+- The subject of the email : {subject}
+- The body of the email : {body}
+- The detected sentiment of the email : {sentiment}
+
+Your task:
+- Generate exactly 3 possible reply suggestions that a support agent can send back.
+- Make the replies polite, professional, and concise.
+- Adapt the tone slightly depending on the sentiment (e.g., empathetic if negative, cheerful if positive).
+- Do not include the subject, sentiment, or any explanations in your output.
+- Only return the reply bodies as a json object, numbered 1 through 3.
+`
+
+const systemPrompt = ChatPromptTemplate.fromTemplate(systemTemp)
+
+const jsonParser = new JsonOutputParser()
+
+const ReplyChain = systemPrompt.pipe(llm).pipe(jsonParser)
+
+export default ReplyChain
+
--- InboxIQ/src/component/Email/EmailDetail.jsx
+++ InboxIQ/src/component/Email/EmailDetail.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
+import ReplyChain from '../../SmartReply'
+
 
 // create and export the chain, invoke once the specified email is clicked 
 function EmailDetail({email}) {
   const [reply, setReply] = useState() // the smart replies we get from the chain 
-  const [loding, setLoading] = useState() // for when the modal is nvoked and response is being generated
+  const [loding, setLoading] = useState() // for when the modal is invoked and response is being generated
+  const[subject, setSubject] = useState()
+  const[] = useState()
+  const[] = useState()
+  
+  async function generateReply() {
+    const resp = await ReplyChain.invoke({
+      // subject, body, sentiment
+    })
+  }
   
 
   return (
